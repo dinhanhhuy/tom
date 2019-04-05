2b968995106d3c62d9294ea6a47a95082e692e63
dahuy <dinhanhhuy.it@gmail.com>
2019-04-05 13:35:56 +0700

    init base project

---
+++ express/api/config/index.js
@@ -0,0 +1,52 @@
+const DotEnv = require('dotenv')
+const env = _getEnv();
+
+let config = {
+    database: {
+        client: env.DATABASE_CLIENT,
+        connection: {
+            host: env.DATABASE_CONNECTION_HOST,
+            port: env.DATABASE_CONNECTION_PORT,
+            user: env.DATABASE_CONNECTION_USER,
+            password: env.DATABASE_CONNECTION_PASSWORD,
+            database: env.DATABASE_CONNECTION_DATABASE,
+        },
+        pool: {
+            max: env.DATABASE_POOL_MAX,
+            min: env.DATABASE_POOL_MIN,
+            acquire: env.DATABASE_POOL_ACQUIRE,
+            idle: env.DATABASE_POOL_IDLE,
+        },
+    }
+};
+
+function _getEnv() {
+    const nodeEnv = process.env.NODE_ENV || 'local';
+    //get env from config file
+    const env = DotEnv.config({path: `${__dirname}/${nodeEnv}.env`}).parsed;
+    //assign direct parameter to env
+    Object.assign(env, process.env);    //direct parameter have high priority then env file
+    //format data
+    const keys = Object.keys(env);
+    keys.forEach(key => {
+        switch (env[key]) {
+            case 'true':
+                env[key] = true;
+                break;
+            case 'false':
+                env[key] = false;
+                break;
+            case 'null':
+                env[key] = null;
+                break;
+            case 'undefined':
+                env[key] = undefined;
+                break;
+            default:
+                break;
+        }
+    });
+    return env;
+}
+
+module.exports = config;
