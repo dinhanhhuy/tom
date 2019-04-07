c005c7e29dda85cb72f12bb42e18af14106a8146
dahuy <dinhanhhuy.it@gmail.com>
2019-04-07 13:00:48 +0700

    update test

--- express/api/test/integration/ex3.js
+++ express/api/test/integration/ex3.js
@@ -2,7 +2,7 @@ const Core = require('../core');
 const moment = require('moment');
 const timeoutApi = 50000;
 
-describe('************** Example 3: **************', () => {
+describe('**************************** Example 3: ****************************', () => {
     result = {};
     it('truncate db', async function () {
         await Core.truncateDB();
--- express/api/test/integration/ex4.js
+++ express/api/test/integration/ex4.js
@@ -2,7 +2,7 @@ const Core = require('../core');
 const moment = require('moment');
 const timeoutApi = 50000;
 
-describe('************** Example 4: **************', () => {
+describe('**************************** Example 4: ****************************', () => {
     result = {};
     it('truncate db', async function () {
         await Core.truncateDB();
